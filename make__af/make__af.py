338d738c343f91cba8d504c193153744a70f82f8
wfw-pgr <wfw.pgr@gmail.com>
2020-09-05 16:08:03 +0900

    modified to allow load__constants

--- make__af/make__af.py
+++ make__af/make__af.py
@@ -10,19 +10,13 @@ def make__af():
     # ------------------------------------------------- #
     # --- [1] Load parameters                       --- #
     # ------------------------------------------------- #
-    
-    a = 11
-    b = 41.13
-    d = 34.955
-    t = 5.0
-    r = 2.5
-    f = 2.856
-    delta = 1.0
-    
-    outFile = "basicCavity.af"
+
+    import nkUtilities.load__constants as lcn
+    cnfFile = "dat/parameter.conf"
+    const   = lcn.load__constants( inpFile=cnfFile )
     
     print()
-    print( "[make__af]  outFile :: {0}".format( outFile ) )
+    print( "[make__af]  outFile :: {0}".format( const["outFile"] ) )
     print()
 
     # ------------------------------------------------- #
@@ -33,40 +27,47 @@ def make__af():
         "### {0} GHz Cavity\n"\
         "### squared shape cavity with a bore tube\n"\
         "### created by K.Nishida (2020/08)\n"\
-        "\n\n".format( f )
+        "\n\n".format( const["frequency"] )
     
     settings = \
-        "&reg kprob=1,    ! superfish problem\n"\
-        "icylin=1,        ! cylindrical geometry\n"\
-        "freq={0},        ! RF Frequency\n"\
-        "dx={1},          ! mesh interval\n"\
-        "xdri=0,ydri=5,   ! Drive point of RF\n"\
-        "kmethod=1,       ! Use beta to compute wave number\n"\
-        "beta=0.95 &      ! Particle velocity for transit-time integrals\n"\
-        "\n\n".format( f, delta )
+        "&reg kprob=1,            ! superfish problem\n"\
+        "icylin=1,                ! cylindrical geometry\n"\
+        "freq={0},                ! RF Frequency\n"\
+        "dx={1},                  ! mesh interval\n"\
+        "xdri={2},ydri={3},       ! Drive point of RF\n"\
+        "kmethod=1,               ! Use beta to compute wave number\n"\
+        "beta={4} &               ! Particle velocity for transit-time integrals\n"\
+        "\n\n".format( const["frequency"]*1000.0, const["delta"], \
+                       const["xy_drive"][0], const["xy_drive"][1], const["beta"] )
 
 
     # ------------------------------------------------- #
     # --- [3] coordinate settings                   --- #
     # ------------------------------------------------- #
 
-    pts      = [ [                 0.0,    0.0 ],
-                 [                 0.0,      b ],
-                 [         0.5*d-0.5*t,      b ],
-                 [         0.5*d-0.5*t,    a+r ],
-                 [         0.5*d+0.5*t,    a+r ],
-                 [         0.5*d+0.5*t,      b ],
-                 [     d + 0.5*d-0.5*t,      b ],
-                 [     d + 0.5*d-0.5*t,    a+r ],
-                 [     d + 0.5*d+0.5*t,    a+r ],
-                 [     d + 0.5*d+0.5*t,      b ],
-                 [ 2.0*d + 0.5*d-0.5*t,      b ],
-                 [ 2.0*d + 0.5*d-0.5*t,    a+r ],
-                 [ 2.0*d + 0.5*d+0.5*t,    a+r ],
-                 [ 2.0*d + 0.5*d+0.5*t,      b ],
-                 [ 3.0*d              ,      b ],
-                 [ 3.0*d              ,    0.0 ],
-                 [                 0.0,    0.0 ]
+    b        = const["cell_radius"]
+    d        = const["cell_length"]
+    a        = const["aperture_radius"]
+    t        = const["aperture_length"]
+    ht       = const["aperture_length"] * 0.5
+    
+    pts      = [ [              0.0,    0.0 ],
+                 [              0.0,      b ],
+                 [         0.5*d-ht,      b ],
+                 [         0.5*d-ht,   a+ht ],
+                 [         0.5*d+ht,   a+ht ],
+                 [         0.5*d+ht,      b ],
+                 [     d + 0.5*d-ht,      b ],
+                 [     d + 0.5*d-ht,   a+ht ],
+                 [     d + 0.5*d+ht,   a+ht ],
+                 [     d + 0.5*d+ht,      b ],
+                 [ 2.0*d + 0.5*d-ht,      b ],
+                 [ 2.0*d + 0.5*d-ht,   a+ht ],
+                 [ 2.0*d + 0.5*d+ht,   a+ht ],
+                 [ 2.0*d + 0.5*d+ht,      b ],
+                 [ 3.0*d           ,      b ],
+                 [ 3.0*d           ,    0.0 ],
+                 [              0.0,    0.0 ]
     ]
     pts      = np.array( pts )
     
@@ -80,7 +81,7 @@ def make__af():
     # --- [4] write in a file                       --- #
     # ------------------------------------------------- #
         
-    with open( outFile, "w" ) as f:
+    with open( const["outFile"], "w" ) as f:
         f.write( comment  )
         f.write( settings )
         f.write( geometry )
